23657a9a963dbd359ca6c4d00ee1dec87ba60153
root <root@raspberrypi>
2019-05-05 18:26:22 -0400

    is able to turn tv on and switch input!

---
+++ test.py
@@ -0,0 +1,24 @@
+import os, re, time
+
+
+cec_resp = os.popen('echo pow 0 | cec-client -s -d 1').read()
+
+status_search = re.finditer(r"(?<=\b(power\sstatus:)\s)(\w+)", cec_resp)
+
+
+for x in status_search:
+    start = int(x.start())
+    end = int(x.end())
+
+
+status = cec_resp[start:end]
+is_on = status == "on"
+
+if not is_on:
+    os.system("echo on 0 | cec-client -s -d 1")
+    time.sleep(1)
+
+
+os.system('echo "as" | cec-client RPI -s -d 1')
+time.sleep(2)
+os.system("omxplayer --no-keys /azan/azan.mp3 &") 
