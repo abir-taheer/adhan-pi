116cbaa6f02a791ea763d3634028c7fde0a8b4ab
Abir Taheer <abir488@gmail.com>
2019-05-05 19:54:55 -0400

    mechanisms work from current experimentation

--- test.py
+++ test.py
@@ -1,21 +1,68 @@
-import json
+import json, time
+from datetime import datetime
+
+
+today = datetime.today().strftime('%Y-%m-%d')
+
 
 def update_times():
     import requests
-    
-
+    api_data = requests.get("http://api.aladhan.com/v1/timingsByCity?city=New+York&country=United+States&method=8").json()
+    ptime = api_data["data"]["timings"]
+    stored_data = {
+        "date": today,
+        "times": {
+            "Fajr": {
+                "time": ptime["Fajr"],
+                "done": False
+            },
+            "Dhuhr": {
+                "time": ptime["Dhuhr"],
+                "done": False
+            },
+            "Asr": {
+                "time": ptime["Asr"],
+                "done": False
+            },
+            "Maghrib": {
+                "time": ptime["Maghrib"],
+                "done": False
+            },
+            "Isha": {
+                "time": ptime["Isha"],
+                "done": False
+            },
+        }
+    }
+    f = open("times.json", "w")
+    f.write(json.dumps(stored_data))
+    f.close()
+    return stored_data
 
 try:
     time_json = open("times.json", "r").read()
     times = json.loads(time_json)
+    assert (times["date"] == today), "Prayer times are not up to date"
 except:
     times = update_times()
 
+adhan_made = True
 
+for prayer in times["times"]:
+    prayer_time = datetime.strptime(times["times"][prayer]["time"], '%H:%M').time()
+    if prayer_time < datetime.today().time():
+        current_prayer = prayer
+        adhan_made = times["times"][prayer]["done"]
 
-exit()
+if adhan_made:
+    exit()
 
-import os, re, time
+times["times"][current_prayer]["done"] = True
+f = open("times.json", "w")
+f.write(json.dumps(times))
+f.close()
+
+import os, re
 
 
 cec_resp = os.popen('echo pow 0 | cec-client -s -d 1').read()
@@ -38,4 +85,8 @@ if not is_on:
 
 os.system('echo "as" | cec-client RPI -s -d 1')
 time.sleep(2)
-os.system("omxplayer --no-keys audio.mp3 &") 
+os.system("omxplayer --no-keys audio.mp3 &")
+
+if not is_on:
+    time.sleep(140)
+    os.system("echo standby 0 | cec-client -s -d 1")
